4e873ceda8e06bbc7add69a327f28de52fffbcb4
javier <javiermdg998@gmail.com>
2020-04-10 12:55:03 +0200

    arreglar modeloski

--- app/models.py
+++ app/models.py
@@ -17,37 +17,45 @@ class Empleado(models.Model):
     telefono = models.CharField(max_length=15)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
+
 class Equipo(models.Model):
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
-    fecha de adquisicion = models.DateField()
-    fecha de instalacion = models.DateField()
-    fecha de ultimo mantenimiento = models.DateField()
+    fecha_adquisicion = models.DateField()
+    fecha_instalacion = models.DateField()
+    fecha_ultimo_mantenimiento = models.DateField()
+
 
 class Tarea(models.Model):
-    hora inicio = models.DateTimeField()
-    hora fin = models.DateTimeField()
+    hora_inicio = models.DateTimeField()
+    hora_fin = models.DateTimeField()
     equipo = models.ForeignKey(Equipo, on_delete=models.set_null)
     proceso = models.ForeignKey(Proceso, on_delete=models.set_null)
     pedido = models.ForeignKey(Pedido, on_delete=models.set_null)
 
+
 class Proceso(models.Model):
     nombre = models.CharField(max_length=100)
+
 
 class AsignacionTarea(models.Model):
     tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
     empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
 
-class Catalogo(models,Model):
+
+class Catalogo(models, Model):
     descripcion = models.CharField(max_length=250)
+
 
 class Pedido(models.Model):
     planifiado = models.BooleanField()
     producido = models.BooleanField()
     cantidad = models.IntegerField()
-    catalogo = models.ForeignKey(Catalogo, on_delete=models.set_null) #set_null por si ya no fabricamos esta pieza, para poder comunicarlo
+    # set_null por si ya no fabricamos esta pieza, para poder comunicarlo
+    catalogo = models.ForeignKey(Catalogo, on_delete=models.set_null)
     cliente = models.ForeignKey(Cliente, on_delete=models.set_null)
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
