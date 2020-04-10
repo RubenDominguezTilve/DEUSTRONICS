0fa10f8d242ce6b022556c5a31410fd527a2211f
javier <javiermdg998@gmail.com>
2020-04-10 13:20:21 +0200

    finaliçazao do modelo

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Usuario(models.Model):
     contrasena = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -26,36 +27,37 @@ class Equipo(models.Model):
     fecha_ultimo_mantenimiento = models.DateField()
 
 
-class Tarea(models.Model):
-    hora_inicio = models.DateTimeField()
-    hora_fin = models.DateTimeField()
-    equipo = models.ForeignKey(Equipo, on_delete=models.SET_NULL)
-    proceso = models.ForeignKey(Proceso, on_delete=models.SET_NULL)
-    pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL)
-
-
 class Proceso(models.Model):
     nombre = models.CharField(max_length=100)
 
 
-class AsignacionTarea(models.Model):
-    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
-    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-
-
-class Catalogo(models, Model):
+class Catalogo(models.Model):
     descripcion = models.CharField(max_length=250)
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 
 class Pedido(models.Model):
     planifiado = models.BooleanField()
     producido = models.BooleanField()
     cantidad = models.IntegerField()
-    # set_null por si ya no fabricamos esta pieza, para poder comunicarlo
-    catalogo = models.ForeignKey(Catalogo, on_delete=models.SET_NULL)
-    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL)
+    # SET_NULL, null=True por si ya no fabricamos esta pieza, para poder comunicarlo
+    catalogo = models.ForeignKey(
+        Catalogo, on_delete=models.SET_NULL, null=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
 
 
-class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+class Tarea(models.Model):
+    hora_inicio = models.DateTimeField()
+    hora_fin = models.DateTimeField()
+    equipo = models.ForeignKey(Equipo, on_delete=models.SET_NULL, null=True)
+    proceso = models.ForeignKey(Proceso, on_delete=models.SET_NULL, null=True)
+    pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True)
+
+
+class AsignacionTarea(models.Model):
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
