04c1b2165380fb94faf6ad0960dff8307b6dc08c
javier <javiermdg998@gmail.com>
2020-04-10 12:09:49 +0200

    modelo bien chingon gei

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    contrasena = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+
+
+class Empleado(models.Model):
+    dni = models.CharField(max_length=9)
