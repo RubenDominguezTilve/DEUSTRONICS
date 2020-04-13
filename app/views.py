852287b7d3ed64c0d2c0fe24a1c14719f78420f2
Unknown <reyes.iker@opendeusto.es>
2020-04-13 11:59:54 +0200

    Importando las clases que importan

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Equipo, Proceso, Pedido
+from .models import Equipo, Proceso, Pedido, Proceso, Tarea
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
