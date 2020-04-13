9b1369a88a9518e71801ee433c010dbbb977bd06
javier <javiermdg998@gmail.com>
2020-04-13 11:37:59 +0200

    vistas pedidos y cambiar nombre do rutas

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('equipos/', views.EquiposListView.as_view(), name='equipos_lista'),
+    path('equipos/', views.EquipoListView.as_view(), name='equipos_lista'),
     path('equipo/<int:pk>/', views.EquipoDetailView.as_view(), name='equipo_detalle'),
+    path('pedidos/', views.PedidoListView.as_view(), name='pedidos_lista'),
+    path('pedido/<int:pk>/', views.PedidoDetailView.as_view(), name='pedido_detalle'),
+
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Equipo
+from .models import Equipo, Proceso, Pedido
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
@@ -9,7 +9,7 @@ def index(req):
     return HttpResponse("funciono de forma moito gostosa!")
 
 
-class EquiposListView(ListView):
+class EquipoListView(ListView):
     model = Equipo
     template_name = 'equipo_lista.html'
     queryset = Equipo.objects.all()
@@ -28,4 +28,26 @@ class EquipoDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(EquipoDetailView, self).get_context_data(**kwargs)
         context['titulo_pagina'] = 'Detalles del Producto'
+        return context
+
+
+class PedidoListView(ListView):
+    model = Pedido
+    template_name = 'pedido_lista.html'
+    queryset = Pedido.objects.all()
+    context_object_name = 'pedidos'
+
+    def get_context_data(self, **kwargs):
+        context = super(PedidoListView, self).get_context_data(**kwargs)
+        # context['titulo_pagina'] = 'Productos'
+        return context
+
+
+class PedidoDetailView(DetailView):
+    model = Pedido
+    template_name = 'pedido_detalle.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PedidoDetailView, self).get_context_data(**kwargs)
+        #context['titulo_pagina'] = 'Detalles del Producto'
         return context
