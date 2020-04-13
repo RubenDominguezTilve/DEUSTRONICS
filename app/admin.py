322c77163ea7288561039d307cd1db65ae271210
Unknown <reyes.iker@opendeusto.es>
2020-04-13 13:25:45 +0200

    Muchos cambios, muchos e, muchísimos, y correcciones e, una locura

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Usuario, Empleado, Equipo, Tarea, Proceso, Pedido, Catalogo, Cliente,TipoEquipo
+from .models import Usuario, Empleado, Equipo, Tarea, Proceso, Pedido, Catalogo, Cliente, TipoEquipo
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Empleado)
