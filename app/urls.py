770bfb74eb65f9c37bb7767795c14d34c8a914f8
javier <javiermdg998@gmail.com>
2020-04-15 12:22:18 +0200

    ?

--- app/urls.py
+++ app/urls.py
@@ -3,14 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('equipos/', views.EquipoListView.as_view(), name='equipos_lista'),
+    path('empleados/', views.EmpleadoListView.as_view(), name='empleado_lista'),
+    path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado_detalle'),
+    path('equipos/', views.EquipoListView.as_view(), name='equipo_lista'),
     path('equipo/<int:pk>/', views.EquipoDetailView.as_view(), name='equipo_detalle'),
-    path('pedidos/', views.PedidoListView.as_view(), name='pedidos_lista'),
+    path('pedidos/', views.PedidoListView.as_view(), name='pedido_lista'),
     path('pedido/<int:pk>/', views.PedidoDetailView.as_view(), name='pedido_detalle'),
-    path('procesos/', views.ProcesoListView.as_view(), name='procesos_lista'),
+    path('procesos/', views.ProcesoListView.as_view(), name='proceso_lista'),
     path('proceso/<int:pk>/', views.ProcesoDetailView.as_view(),
          name='proceso_detalle'),
-    path('tareas/', views.TareaListView.as_view(), name='tareas_lista'),
+    path('tareas/', views.TareaListView.as_view(), name='tarea_lista'),
     path('tarea/<int:pk>/', views.TareaDetailView.as_view(), name='tarea_detalle'),
 
 
