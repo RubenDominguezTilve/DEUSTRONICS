1e910734c8b80dd89abde37c2e62893a03f00e04
javier <javiermdg998@gmail.com>
2020-04-27 18:58:29 +0200

    saber el tipo de user volumen 1

--- app/sessionHandler.py
+++ app/sessionHandler.py
@@ -1,7 +1,21 @@
 from .models import Empleado, Cliente
-
+from .consts import OPERARIO,RESPONABLE,CLIENTE,SUPERUSER
 def getLoggedEmpleado(req):
     return Empleado.objects.filter(usuario=req.user.id)[0]
 
 def getLoggedCliente(req):
     return Cliente.objects.filter(usuario=req.user.id)[0]
+
+def getTipoUsuario(req):
+    tipo=OPERARIO
+    if req.user.groups.filter(name=CLIENTE).exists():
+        tipo=CLIENTE
+
+    if req.user.groups.filter(name=OPERARIO).exists():
+        tipo=OPERARIO
+
+    if req.user.groups.filter(name=RESPONABLE).exists():
+        tipo=RESPONABLE
+    if req.user.groups.filter(name=SUPERUSER).exists():
+        tipo=SUPERUSER
+    return tipo
