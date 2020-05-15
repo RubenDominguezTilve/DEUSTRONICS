484ca1dbcb6fd8fe0e272e13bd620566f3d72b67
Unknown <ruben.dominguez@opendeusto.es>
2020-05-15 14:21:22 +0200

    Aquí Willyrex comentando

--- app/static/js/misTareas.js
+++ app/static/js/misTareas.js
@@ -1,14 +1,19 @@
 var checks = document.getElementsByClassName("check-finalizar");
 
+//Recorremos todos los checkbox que tengan class="check-finalizar" (uno por tarea, en principio)
 for (var i = 0; i < checks.length; i++) {
+  //Añadimos un EventListener para poder saber cuando se clica el checkbox
   checks[i].addEventListener("click", function (event) {
+    //Definimos el formulario para luego acceder a sus datos
     let form = event.target.parentNode;
-    console.log(form);
+    //Definimos la tarea para luego actualizarla en BBDD
     let tarea = new FormData(form);
-    console.log(tarea.get("idtarea"));
+    //Definimos las fila que corresponde a la tarea para despues eliminarla
     let row = form.parentNode.parentNode;
+    //La marcamos como done, lo que la hace moverse via CSS
     row.classList.add("tarea-done");
 
+    //Definimos el tiempo que se va a desplazar antes de eliminarla
     setTimeout(function () {
       row.remove();
     }, 2000);
