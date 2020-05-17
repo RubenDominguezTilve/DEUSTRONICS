06195b68d7a6a64206b91094f489c5172a9c316a
javier <javiermdg998@gmail.com>
2020-05-17 21:21:06 +0200

    animasao do traseiro

--- app/static/js/catalogo.js
+++ app/static/js/catalogo.js
@@ -1,9 +1,10 @@
 var datajson;
 function lista_productos(data) {
   let cad = "";
-  console.log(data);
-  for (let element of data) {
-    cad += `<p>${element.nombre}</p>`;
+  console.log(data.length);
+
+  for (let i = 0; i < data.length; i++) {
+    cad += template_catalogo(data[i], i);
   }
   return cad;
 }
@@ -23,3 +24,44 @@ fetch("../api/catalogo")
   .catch(function (error) {
     console.log(error);
   });
+
+function template_catalogo(producto, i) {
+  return ` 
+      <div class="item catalog-item mb-2 col-sm-3" style="animation-delay:${0.2 * i}s;" >
+      <div class="pad15">
+        <!-- Imagen del producto -->
+        <img src="https://dummyimage.com/600x400/000/fff" class="img-fluid rounded mx-auto d-block" alt="Imagen de catálogo" />
+        <!-- Nombre del producto -->
+        <p class="lead mt-2">${producto.nombre}</p>
+        <!-- Descripción del producto 
+        <p>Descripción: ${producto.descripcion}</p>-->
+        <!-- Detalle del producto -->
+        <a href="${producto.id}"> Ver detalle</a>
+        <!-- Precio del producto por unidad -->
+        <p>Precio: ${producto.precio} €</p>
+        
+
+
+
+
+        <!-- Cantidad de unidades a pedir -->
+        <div class="input-group mb-3">
+          <form  action="../pedido/create/" method="POST">
+         
+            <input type="hidden" name="csrfmiddlewaretoken" value="${csrftoken}">
+            <span class="input-group-text FormGroupPedido" >Cantidad</span>
+            
+            <input type="hidden" name="producto" value="${producto.id}" />
+            <input type="number" class="form-control FormGroupPedido" aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default" name="cantidad" />
+            <!-- Apéndice para el botón de realizar pedido -->
+            <button class="btn btn-outline-dark bg-secondary FormGroupPedido" type="submit" id="button-addon1">Pedir</button>
+            
+            <hr>
+           
+            <p>Precio total: <span>${producto.precio}</span> €</p>
+          </form>
+        </div>       
+      </div>
+    </div>`;
+}
